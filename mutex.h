2774cc911902f72222c720aaa4f3d5d725d02250
dany <daniel.sava99@protonmail.com>
2020-01-13 20:21:34 +0200

    rwlocks

--- mutex.h
+++ mutex.h
@@ -10,6 +10,7 @@ int lock_aquire(lock *mutex) {
     while (__sync_val_compare_and_swap(&mutex->available, 0, 1) != 0);
     return 0;
 }
+
 int lock_release(lock *mutex) {
     mutex->available = 0;
     return 0;
--- rwlocks.c
+++ rwlocks.c
@@ -5,35 +5,39 @@ semaphore mutex = 1;
 int read_count = 0;
 
 void writer() {
-	do {
-		wait(rw_mutex);
+	wait(rw_mutex);
 
-		/* WRITE */
+	/* WRITE */
+	FILE *file_to_write = fopen("my_file.txt", "a");
+	fprintf("Data written by the write");
+	fclose(f);
+	/* END */
 
-		signal(rw_mutex);
-	} while (true);
+	signal(rw_mutex);
 }
 
 
 void reader() {
-	do {
-		wait(mutex);
-		read_count++;
-		if (read_count == 1)
-			wait(rw_mutex);
-		signal(mutex);
+	wait(mutex);
+	read_count++;
+	if (read_count == 1)
+		wait(rw_mutex);
+	signal(mutex);
 
-		/* READ */
+	/* READ */
+	FILE *file_to_read = fopen("my_file.txt", "r");
+	fprintf(file_to_read, "Data read by the reader!\n");
+	sleep(1);
+	fclose(file_to_read);
 
-		wait(mutex);
-		read_count--;
-		if (read_count == 0)
-			signal(rw_mutex);
-	} while (true);
+	wait(mutex);
+	read_count--;
+	if (read_count == 0)
+		signal(rw_mutex);
 }
 
 
 int main() {
-	
+
 	return 0;
 }
